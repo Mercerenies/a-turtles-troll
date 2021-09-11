aa8be3c25491a96a04ace8e48d0532ca601d5891
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2021-09-10 21:08:47 -0400

    DragonBombManager TNT now explodes on contact

--- src/DragonBombManager.kt
+++ src/DragonBombManager.kt
@@ -14,6 +14,7 @@ import org.bukkit.Bukkit
 import org.bukkit.Material
 import org.bukkit.Chunk
 import org.bukkit.World
+import org.bukkit.NamespacedKey
 import org.bukkit.entity.Entity
 import org.bukkit.entity.EnderDragon
 import org.bukkit.entity.TNTPrimed
@@ -26,6 +27,7 @@ import org.bukkit.event.block.BlockPlaceEvent
 import org.bukkit.event.entity.EntityDamageEvent
 import org.bukkit.potion.PotionEffect
 import org.bukkit.potion.PotionEffectType
+import org.bukkit.persistence.PersistentDataType
 
 import kotlin.random.Random
 
@@ -33,23 +35,30 @@ class DragonBombManager(val plugin: Plugin) : RunnableFeature(), Listener {
 
   companion object {
     val TICKS_PER_SECOND = 20L
+    val TIMER_TRIGGERS_PER_ATTACK = 12L
+    val BOMB_MARKER_KEY = "dragon_bomb_manager_tag"
+    val VELOCITY_EPSILON = 0.0001
   }
+
+  private var timerTick = 0L
+  private val cooldownMemory = CooldownMemory<TNTPrimed>(plugin)
 
   override val name = "dragonbomb"
 
   override val description = "The Ender Dragon drops TNT regularly"
 
+  val markerKey = NamespacedKey(plugin, BOMB_MARKER_KEY)
+
   override fun run() {
     if (!isEnabled()) {
       return
     }
-    val server = Bukkit.getServer()
-    for (world in server.getWorlds()) {
-      for (dragon in world.getEntitiesByClass(EnderDragon::class.java)) {
-        val tnt = world.spawn(dragon.getLocation(), TNTPrimed::class.java)
-        tnt.setFuseTicks((TICKS_PER_SECOND * 3).toInt())
-      }
+    timerTick += 1L
+    if (timerTick >= TIMER_TRIGGERS_PER_ATTACK) {
+      timerTick = 0L
+      doDragonAttack()
     }
+    checkAllTNT()
   }
 
   @EventHandler
@@ -70,7 +79,40 @@ class DragonBombManager(val plugin: Plugin) : RunnableFeature(), Listener {
   }
 
   fun register() {
-    this.runTaskTimer(plugin, 1L, TICKS_PER_SECOND * 3L)
+    this.runTaskTimer(plugin, 1L, 5L)
+  }
+
+  private fun doDragonAttack() {
+    val server = Bukkit.getServer()
+    for (world in server.getWorlds()) {
+      for (dragon in world.getEntitiesByClass(EnderDragon::class.java)) {
+        val tnt = world.spawn(dragon.getLocation(), TNTPrimed::class.java)
+        tnt.setFuseTicks((TICKS_PER_SECOND * 10).toInt())
+
+        val container = tnt.getPersistentDataContainer()
+        container.set(markerKey, PersistentDataType.INTEGER, 1)
+
+        cooldownMemory.add(tnt, 4L)
+
+      }
+    }
+  }
+
+  private fun checkAllTNT() {
+    // Any TNT with the marker key set which is not moving should
+    // explode.
+    val server = Bukkit.getServer()
+    for (world in server.getWorlds()) {
+      for (tnt in world.getEntitiesByClass(TNTPrimed::class.java)) {
+        if (!cooldownMemory.contains(tnt)) {
+          if (tnt.getVelocity().length() <= VELOCITY_EPSILON) {
+            if (tnt.getPersistentDataContainer().get(markerKey, PersistentDataType.INTEGER) == 1) {
+              tnt.setFuseTicks(1) // Go boom :)
+            }
+          }
+        }
+      }
+    }
   }
 
 }
