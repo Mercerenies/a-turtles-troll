bc30601d7ace8e811f10ec4c79e9420888f1e498
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2021-10-20 20:20:01 -0400

    RecipeFeature common superclass

--- src/recipe/AnvilRecipeFeature.kt
+++ src/recipe/AnvilRecipeFeature.kt
@@ -2,19 +2,18 @@
 package com.mercerenies.turtletroll.recipe
 
 import com.mercerenies.turtletroll.feature.AbstractFeature
+
 import org.bukkit.plugin.Plugin
 import org.bukkit.Bukkit
 import org.bukkit.NamespacedKey
 import org.bukkit.Material
+import org.bukkit.inventory.Recipe
 import org.bukkit.inventory.FurnaceRecipe
 import org.bukkit.inventory.ItemStack
 
-class AnvilRecipeFeature(val plugin: Plugin) : AbstractFeature() {
+class AnvilRecipeFeature(plugin: Plugin) : RecipeFeature(plugin) {
 
   companion object {
-    val RECIPE_KEY0 = "anvil_smelt"
-    val RECIPE_KEY1 = "anvil_smelt1"
-    val RECIPE_KEY2 = "anvil_smelt2"
     val TICKS_PER_SECOND = 20
   }
 
@@ -22,41 +21,11 @@ class AnvilRecipeFeature(val plugin: Plugin) : AbstractFeature() {
 
   override val description = "Anvils can be smelted"
 
-  val namespacedKey0 = NamespacedKey(plugin, RECIPE_KEY0)
-  val namespacedKey1 = NamespacedKey(plugin, RECIPE_KEY1)
-  val namespacedKey2 = NamespacedKey(plugin, RECIPE_KEY2)
-
-  override fun enable() {
-    if (!isEnabled()) {
-      addRecipes()
-    }
-    super.enable()
-  }
-
-  override fun disable() {
-    if (isEnabled()) {
-      removeRecipes()
-    }
-    super.disable()
-  }
-
-  fun addRecipes() {
-
-    val recipe0 = FurnaceRecipe(namespacedKey0, ItemStack(Material.IRON_NUGGET), Material.ANVIL, 0.2f, TICKS_PER_SECOND * 10)
-    Bukkit.addRecipe(recipe0)
-
-    val recipe1 = FurnaceRecipe(namespacedKey1, ItemStack(Material.IRON_NUGGET), Material.CHIPPED_ANVIL, 0.2f, TICKS_PER_SECOND * 10)
-    Bukkit.addRecipe(recipe1)
-
-    val recipe2 = FurnaceRecipe(namespacedKey2, ItemStack(Material.IRON_NUGGET), Material.DAMAGED_ANVIL, 0.2f, TICKS_PER_SECOND * 10)
-    Bukkit.addRecipe(recipe2)
-
-  }
-
-  fun removeRecipes() {
-    Bukkit.removeRecipe(namespacedKey0)
-    Bukkit.removeRecipe(namespacedKey1)
-    Bukkit.removeRecipe(namespacedKey2)
+  override fun getRecipes(): List<UnkeyedRecipe<Recipe>> {
+    val recipe0 = UnkeyedRecipe<Recipe> { FurnaceRecipe(it, ItemStack(Material.IRON_NUGGET), Material.ANVIL, 0.2f, TICKS_PER_SECOND * 10) }
+    val recipe1 = UnkeyedRecipe<Recipe> { FurnaceRecipe(it, ItemStack(Material.IRON_NUGGET), Material.CHIPPED_ANVIL, 0.2f, TICKS_PER_SECOND * 10) }
+    val recipe2 = UnkeyedRecipe<Recipe> { FurnaceRecipe(it, ItemStack(Material.IRON_NUGGET), Material.DAMAGED_ANVIL, 0.2f, TICKS_PER_SECOND * 10) }
+    return listOf(recipe0, recipe1, recipe2)
   }
 
 }
---
+++ src/recipe/RecipeFeature.kt
@@ -0,0 +1,65 @@
+
+package com.mercerenies.turtletroll.recipe
+
+import com.mercerenies.turtletroll.feature.AbstractFeature
+
+import org.bukkit.plugin.Plugin
+import org.bukkit.Bukkit
+import org.bukkit.NamespacedKey
+import org.bukkit.Material
+import org.bukkit.inventory.Recipe
+
+import kotlin.reflect.KClass
+
+abstract class RecipeFeature(val plugin: Plugin) : AbstractFeature() {
+
+  companion object {
+
+    fun keyName(cls: KClass<out Any>, index: Int): String =
+      "${cls.qualifiedName}.${index}"
+
+  }
+
+  fun interface UnkeyedRecipe<out T : Recipe> {
+    operator fun invoke(key: NamespacedKey): T
+  }
+
+  private var recipeCount: Int = 0
+
+  private fun namespacedKey(index: Int): NamespacedKey =
+    NamespacedKey(plugin, keyName(this::class, index))
+
+  abstract fun getRecipes(): List<UnkeyedRecipe<Recipe>>
+
+  override fun enable() {
+    if (!isEnabled()) {
+      addRecipes()
+    }
+    super.enable()
+  }
+
+  override fun disable() {
+    if (isEnabled()) {
+      removeRecipes()
+    }
+    super.disable()
+  }
+
+  fun addRecipes() {
+    val recipes = getRecipes()
+    recipes.forEachIndexed { index, recipe ->
+      val compiledRecipe = recipe(namespacedKey(index))
+      Bukkit.addRecipe(compiledRecipe)
+    }
+
+    recipeCount = recipes.size
+  }
+
+  fun removeRecipes() {
+    (0..recipeCount-1).forEach {
+      Bukkit.removeRecipe(namespacedKey(it))
+    }
+    recipeCount = 0
+  }
+
+}
