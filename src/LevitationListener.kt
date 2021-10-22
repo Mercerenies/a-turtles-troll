499709d1e9ce0fe3b30b0a2014a86a41ae515476
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2021-10-21 21:29:03 -0400

    More BlockTypes

--- src/LevitationListener.kt
+++ src/LevitationListener.kt
@@ -12,17 +12,6 @@ import org.bukkit.potion.PotionEffectType
 
 class LevitationListener : AbstractFeature(), Listener {
 
-  companion object {
-    val BLOCKS = setOf(
-      Material.COAL_ORE, Material.IRON_ORE, Material.LAPIS_ORE,
-      Material.GOLD_ORE, Material.DIAMOND_ORE, Material.EMERALD_ORE,
-      Material.NETHER_QUARTZ_ORE, Material.NETHER_GOLD_ORE, Material.ANCIENT_DEBRIS,
-      Material.COPPER_ORE, Material.DEEPSLATE_COAL_ORE, Material.DEEPSLATE_COPPER_ORE,
-      Material.DEEPSLATE_DIAMOND_ORE, Material.DEEPSLATE_EMERALD_ORE, Material.DEEPSLATE_GOLD_ORE,
-      Material.DEEPSLATE_IRON_ORE, Material.DEEPSLATE_LAPIS_ORE, Material.DEEPSLATE_REDSTONE_ORE,
-    )
-  }
-
   override val name = "levitation"
 
   override val description = "Certain blocks cause the player to levitate"
@@ -35,7 +24,7 @@ class LevitationListener : AbstractFeature(), Listener {
     val loc = event.getTo()
     if (loc != null) {
       val block = loc.clone().add(0.0, -1.0, 0.0).getBlock()
-      if (BLOCKS.contains(block.type)) {
+      if (BlockTypes.ORES.contains(block.type)) {
         val player = event.player
         player.addPotionEffect(PotionEffect(PotionEffectType.LEVITATION, Constants.TICKS_PER_SECOND * 5, 0))
       }
