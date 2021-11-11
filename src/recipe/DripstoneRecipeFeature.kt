aa80f02beb06ed956e6e994bbb482ef5bb403eda
Evanski <61751485+EvanSkiStudios@users.noreply.github.com>
2021-11-10 21:50:35 -0500

    support for #25 for datapack
    Code to add custom name to crafted dripstone arrows so the datapack knows that item to effect

--- src/recipe/DripstoneRecipeFeature.kt
+++ src/recipe/DripstoneRecipeFeature.kt
@@ -8,6 +8,7 @@ import org.bukkit.Material
 import org.bukkit.inventory.Recipe
 import org.bukkit.inventory.ShapedRecipe
 import org.bukkit.inventory.ItemStack
+import org.bukkit.persistence.PersistentDataType
 
 class DripstoneRecipeFeature(plugin: Plugin) : RecipeFeature(plugin) {
 
@@ -16,8 +17,19 @@ class DripstoneRecipeFeature(plugin: Plugin) : RecipeFeature(plugin) {
   override val description = "Dripstone can be used in place of flint when making arrows"
 
   override fun getRecipes(): List<UnkeyedRecipe<Recipe>> {
+
     val recipe = UnkeyedRecipe<Recipe> { key: NamespacedKey ->
-      val recipe = ShapedRecipe(key, ItemStack(Material.ARROW, 4))
+
+      //Evanski's horrible code here that gives the arrows custom names he can get in the datapack
+      val ud_arrow = ItemStack(Material.ARROW, 4)
+      val ud_meta = ud_arrow.getItemMeta()!!
+
+      ud_meta.setDisplayName("Upside Down Arrow")
+      ud_meta.setCustomModelData(8565)
+      ud_arrow.setItemMeta(ud_meta)
+      //code continues as normal
+
+      val recipe = ShapedRecipe(key, ud_arrow)
       recipe.shape("A",
                    "B",
                    "C")
