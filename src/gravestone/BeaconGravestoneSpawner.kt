d58d3dd5e447a5809c24ef087e4ac677b1ee35fc
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2022-03-31 15:26:22 -0400

    Fixed directions on beacon

--- src/gravestone/BeaconGravestoneSpawner.kt
+++ src/gravestone/BeaconGravestoneSpawner.kt
@@ -6,24 +6,24 @@ import org.bukkit.block.BlockFace
 import org.bukkit.block.Sign
 import org.bukkit.block.`data`.`type`.Bed
 import org.bukkit.block.`data`.`type`.WallSign
+import org.bukkit.block.`data`.`type`.Slab
 import org.bukkit.block.`data`.Bisected
 import org.bukkit.Material
 
-///// The others
 object BeaconGravestoneSpawner : GravestoneSpawner() {
 
   override fun spawnGravestone(centerBlock: Block, inscriptions: Inscriptions, rotation: Rotation) {
 
     // The stone itself
-    //replaceWith(centerBlock, Material.STONE)
-    replaceWith(centerBlock.location.clone().add(rotation.vector(0, 1, 0)).block, Material.STONE)
-    //replaceWith(centerBlock.location.clone().add(rotation.vector(0, 2, 0)).block, Material.STONE)
-    replaceWithStair(centerBlock.location.clone().add(rotation.vector(0, 0, 1)).block, Material.STONE_STAIRS, Bisected.Half.BOTTOM, BlockFace.NORTH)
-    replaceWithStair(centerBlock.location.clone().add(rotation.vector(0, 1, 1)).block, Material.STONE_STAIRS, Bisected.Half.TOP, BlockFace.NORTH)
-    replaceWithStair(centerBlock.location.clone().add(rotation.vector(0, 2, 1)).block, Material.STONE_STAIRS, Bisected.Half.BOTTOM, BlockFace.NORTH)
-    replaceWithStair(centerBlock.location.clone().add(rotation.vector(0, 0, -1)).block, Material.STONE_STAIRS, Bisected.Half.BOTTOM, BlockFace.SOUTH)
-    replaceWithStair(centerBlock.location.clone().add(rotation.vector(0, 1, -1)).block, Material.STONE_STAIRS, Bisected.Half.TOP, BlockFace.SOUTH)
-    replaceWithStair(centerBlock.location.clone().add(rotation.vector(0, 2, -1)).block, Material.STONE_STAIRS, Bisected.Half.BOTTOM, BlockFace.SOUTH)
+    //replaceWithStone(centerBlock)
+    replaceWithStone(centerBlock.location.clone().add(rotation.vector(0, 1, 0)).block)
+    //replaceWithStone(centerBlock.location.clone().add(rotation.vector(0, 2, 0)).block)
+    replaceWithStair(centerBlock.location.clone().add(rotation.vector(0, 0, 1)).block, Bisected.Half.BOTTOM, BlockFace.SOUTH)
+    replaceWithStair(centerBlock.location.clone().add(rotation.vector(0, 1, 1)).block, Bisected.Half.TOP, BlockFace.SOUTH)
+    replaceWithStair(centerBlock.location.clone().add(rotation.vector(0, 2, 1)).block, Bisected.Half.BOTTOM, BlockFace.SOUTH)
+    replaceWithStair(centerBlock.location.clone().add(rotation.vector(0, 0, -1)).block, Bisected.Half.BOTTOM, BlockFace.NORTH)
+    replaceWithStair(centerBlock.location.clone().add(rotation.vector(0, 1, -1)).block, Bisected.Half.TOP, BlockFace.NORTH)
+    replaceWithStair(centerBlock.location.clone().add(rotation.vector(0, 2, -1)).block, Bisected.Half.BOTTOM, BlockFace.NORTH)
 
     // Now the sign
     val signBlock = centerBlock.location.clone().add(rotation.vector(1, 1, 0)).block
