ea96e25cc0fc878fb7ff78f21bc9132d25276bc6
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2022-04-21 16:45:38 -0400

    CompositeFeatureContainer

---
+++ src/feature/CompositeFeatureContainer.kt
@@ -0,0 +1,34 @@
+
+package com.mercerenies.turtletroll.feature
+
+import com.mercerenies.turtletroll.recipe.RecipeFeature
+import com.mercerenies.turtletroll.recipe.RecipeDeleter
+import com.mercerenies.turtletroll.command.Command
+import com.mercerenies.turtletroll.command.PermittedCommand
+import com.mercerenies.turtletroll.util.lazyFlatten
+
+import org.bukkit.event.Listener
+
+class CompositeFeatureContainer(
+  private val allContainers: Iterable<FeatureContainer>,
+) : FeatureContainer {
+
+  override val listeners: Iterable<Listener> =
+    allContainers.map { it.listeners }.lazyFlatten()
+
+  override val features: Iterable<Feature> =
+    allContainers.map { it.features }.lazyFlatten()
+
+  override val runnables: Iterable<RunnableFeature> =
+    allContainers.map { it.runnables }.lazyFlatten()
+
+  override val recipes: Iterable<RecipeFeature> =
+    allContainers.map { it.recipes }.lazyFlatten()
+
+  override val recipeDeleters: Iterable<RecipeDeleter> =
+    allContainers.map { it.recipeDeleters }.lazyFlatten()
+
+  override val commands: Iterable<Pair<String, PermittedCommand<Command>>> =
+    allContainers.map { it.commands }.lazyFlatten()
+
+}
