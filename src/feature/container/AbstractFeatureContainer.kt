fde1001de0d4e6db1b1a9590a51403060dceae69
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2022-04-22 14:55:21 -0400

    AbstractFeatureContainer

---
+++ src/feature/container/AbstractFeatureContainer.kt
@@ -0,0 +1,35 @@
+
+package com.mercerenies.turtletroll.feature.container
+
+import com.mercerenies.turtletroll.feature.Feature
+import com.mercerenies.turtletroll.feature.RunnableFeature
+import com.mercerenies.turtletroll.recipe.RecipeFeature
+import com.mercerenies.turtletroll.recipe.RecipeDeleter
+import com.mercerenies.turtletroll.command.Command
+import com.mercerenies.turtletroll.command.PermittedCommand
+
+import org.bukkit.event.Listener
+
+// Abstract class version of FeatureContainer which has trivially
+// empty implementations for all methods
+abstract class AbstractFeatureContainer : FeatureContainer {
+
+  open override val listeners: Iterable<Listener>
+    get() = listOf()
+
+  open override val features: Iterable<Feature>
+    get() = listOf()
+
+  open override val runnables: Iterable<RunnableFeature>
+    get() = listOf()
+
+  open override val recipes: Iterable<RecipeFeature>
+    get() = listOf()
+
+  open override val recipeDeleters: Iterable<RecipeDeleter>
+    get() = listOf()
+
+  open override val commands: Iterable<Pair<String, PermittedCommand<Command>>>
+    get() = listOf()
+
+}
