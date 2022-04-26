118d8fe83b2049abc3336f3fc86b4f57e1b0f664
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2022-04-26 16:22:18 -0400

    Shared data

--- src/feature/builder/BuilderState.kt
+++ src/feature/builder/BuilderState.kt
@@ -5,9 +5,16 @@ import com.mercerenies.turtletroll.storage.GlobalDataHolder
 
 import org.bukkit.plugin.Plugin
 
+import kotlin.reflect.KClass
+
 // A feature builder has access to the plugin it's building for, as
 // well as some persistent data store.
 interface BuilderState {
   val plugin: Plugin
   val dataStore: GlobalDataHolder
+
+  fun registerSharedData(key: String, value: Any)
+
+  fun<T : Any> getSharedData(key: String, targetType: KClass<T>): T?
+
 }
--- src/feature/builder/SimpleBuilderState.kt
+++ src/feature/builder/SimpleBuilderState.kt
@@ -5,7 +5,27 @@ import com.mercerenies.turtletroll.storage.GlobalDataHolder
 
 import org.bukkit.plugin.Plugin
 
+import kotlin.collections.HashMap
+import kotlin.reflect.KClass
+import kotlin.reflect.safeCast
+
 class SimpleBuilderState(
   override val plugin: Plugin,
   override val dataStore: GlobalDataHolder,
-) : BuilderState
+) : BuilderState {
+
+  private val storage: HashMap<String, Any> = HashMap()
+
+  override fun registerSharedData(key: String, value: Any) {
+    storage.put(key, value)
+  }
+
+  override fun<T : Any> getSharedData(key: String, targetType: KClass<T>): T? {
+    val value = storage.get(key)
+    if (value == null) {
+      return null
+    }
+    return targetType.safeCast(value)
+  }
+
+}
