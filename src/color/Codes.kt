228c02f99216d863441d6ae6a3f57713bfd02ff3
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2022-05-16 14:38:34 -0400

    Color codes

---
+++ src/color/Codes.kt
@@ -0,0 +1,30 @@
+
+package com.mercerenies.turtletroll.color
+
+object Codes {
+
+  val SIGIL        = "§"
+  val BLACK        = "§0"
+  val DARK_BLUE    = "§1"
+  val DARK_GREEN   = "§2"
+  val DARK_AQUA    = "§3"
+  val DARK_RED     = "§4"
+  val DARK_PURPLE  = "§5"
+  val GOLD         = "§6"
+  val GRAY         = "§7"
+  val DARK_GRAY    = "§8"
+  val BLUE         = "§9"
+  val GREEN        = "§a"
+  val AQUA         = "§b"
+  val RED          = "§c"
+  val LIGHT_PURPLE = "§d"
+  val YELLOW       = "§e"
+  val WHITE        = "§f"
+  val OBFUSCATED   = "§k"
+  val BOLD         = "§l"
+  val STRIKE       = "§m"
+  val UNDERLINE    = "§n"
+  val ITALIC       = "§o"
+  val RESET        = "§r"
+
+}
