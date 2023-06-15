3332b9be24f44025d59cb309732c7481e3ef21a9
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2023-06-15 15:35:09 -0400

    Remove some unnecessary code from gradle file

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,10 +37,10 @@ tasks {
   }
   jar {
     val dependencies = configurations
-        .runtimeClasspath
-        .get()
-        .filter { !it.name.contains("spigot") }
-        .map(::zipTree)
+      .runtimeClasspath
+      .get()
+      .filter { !it.name.contains("spigot") }
+      .map(::zipTree)
     from(dependencies)
     duplicatesStrategy = DuplicatesStrategy.EXCLUDE
     manifest {
@@ -83,15 +83,4 @@ dependencies {
     }
   )
 
-  // This dependency is used internally, and not exposed to consumers on their own compile classpath.
-  //implementation("com.google.guava:guava:30.1.1-jre")
-
-  // Use the Kotlin test library.
-  //testImplementation("org.jetbrains.kotlin:kotlin-test")
-
-  // Use the Kotlin JUnit integration.
-  //testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
-
-  // This dependency is exported to consumers, that is to say found on their compile classpath.
-  //api("org.apache.commons:commons-math3:3.6.1")
 }
