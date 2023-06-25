0814dbefd6b085cd8db9f12e27636bff67f9a8f0
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2023-06-25 19:49:45 -0400

    Fixed wood types on bee attack

--- src/drop/nearby/BeeAttackAction.kt
+++ src/drop/nearby/BeeAttackAction.kt
@@ -2,6 +2,7 @@
 package com.mercerenies.turtletroll.drop.nearby
 
 import com.mercerenies.turtletroll.ext.*
+import com.mercerenies.turtletroll.BlockTypes
 
 import org.bukkit.Material
 import org.bukkit.Location
@@ -14,16 +15,12 @@ class BeeAttackAction(
 ) : NearbyAction {
 
   companion object {
-    val BLOCKS = setOf(
-      Material.OAK_LOG, Material.OAK_PLANKS, Material.STRIPPED_OAK_LOG, Material.STRIPPED_OAK_WOOD,
-      Material.SPRUCE_LOG, Material.SPRUCE_PLANKS, Material.STRIPPED_SPRUCE_LOG, Material.STRIPPED_SPRUCE_WOOD,
-      Material.JUNGLE_LOG, Material.JUNGLE_PLANKS, Material.STRIPPED_JUNGLE_LOG, Material.STRIPPED_JUNGLE_WOOD,
-      Material.BIRCH_LOG, Material.BIRCH_PLANKS, Material.STRIPPED_BIRCH_LOG, Material.STRIPPED_BIRCH_WOOD,
-      Material.ACACIA_LOG, Material.ACACIA_PLANKS, Material.STRIPPED_ACACIA_LOG, Material.STRIPPED_ACACIA_WOOD,
-      Material.DARK_OAK_LOG, Material.DARK_OAK_PLANKS, Material.STRIPPED_DARK_OAK_LOG, Material.STRIPPED_DARK_OAK_WOOD,
-      Material.CRIMSON_HYPHAE, Material.CRIMSON_PLANKS, Material.CRIMSON_STEM, Material.STRIPPED_CRIMSON_HYPHAE, Material.STRIPPED_CRIMSON_STEM,
-      Material.WARPED_HYPHAE, Material.WARPED_PLANKS, Material.WARPED_STEM, Material.WARPED_HYPHAE, Material.WARPED_STEM,
-    )
+    val BLOCKS =
+      BlockTypes.LOGS + BlockTypes.PLANKS +
+        setOf(
+          Material.CRIMSON_HYPHAE, Material.CRIMSON_PLANKS, Material.CRIMSON_STEM, Material.STRIPPED_CRIMSON_HYPHAE, Material.STRIPPED_CRIMSON_STEM,
+          Material.WARPED_HYPHAE, Material.WARPED_PLANKS, Material.WARPED_STEM, Material.WARPED_HYPHAE, Material.WARPED_STEM,
+        )
   }
 
   override fun shouldTrigger(event: BlockBreakEvent): Boolean =
