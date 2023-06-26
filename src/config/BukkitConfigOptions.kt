d31d137b19b14babf0e6c5bf20a1ae28c908fff7
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2023-06-25 20:02:08 -0400

    start of cfg

--- src/config/BukkitConfigOptions.kt
+++ src/config/BukkitConfigOptions.kt
@@ -2,11 +2,24 @@
 package com.mercerenies.turtletroll.config
 
 import org.bukkit.configuration.ConfigurationSection
+import org.bukkit.configuration.file.YamlConfiguration
+
+import java.io.InputStreamReader
 
 // Configuration backed by a Bukkit configuration object
 class BukkitConfigOptions(
   private val config: ConfigurationSection,
 ) : ConfigOptions {
+
+  companion object {
+
+    fun fromCurrentJar(path: String): BukkitConfigOptions =
+      BukkitConfigOptions::class.java.getResourceAsStream(path).use { stream ->
+        val reader = InputStreamReader(stream)
+        BukkitConfigOptions(YamlConfiguration.loadConfiguration(reader))
+      }
+
+  }
 
   override fun<T> getValue(classType: Class<T>, path: String): T? =
     classType.fromBukkitConfig(config, path)
