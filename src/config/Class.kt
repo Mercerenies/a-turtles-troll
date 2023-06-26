db3920c1258eb2c749f44388f3c1229243a306f9
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2023-06-26 16:23:41 -0400

    More accessors

--- src/config/Class.kt
+++ src/config/Class.kt
@@ -5,12 +5,34 @@ import org.bukkit.configuration.ConfigurationSection
 
 interface Class<T> {
 
+  object BOOLEAN : Class<Boolean> {
+    override fun fromBukkitConfig(config: ConfigurationSection, path: String): Boolean? =
+      if (config.isBoolean(path)) {
+        config.getBoolean(path)
+      } else {
+        null
+      }
+  }
+
   object INT : Class<Int> {
     override fun fromBukkitConfig(config: ConfigurationSection, path: String): Int? =
       if (config.isInt(path)) {
         config.getInt(path)
+      } else if (config.isLong(path)) {
+        // Coerce from Long
+        config.getLong(path).toInt()
       } else {
         null
+      }
+  }
+
+  object DOUBLE : Class<Double> {
+    override fun fromBukkitConfig(config: ConfigurationSection, path: String): Double? =
+      if (config.isDouble(path)) {
+        config.getDouble(path)
+      } else {
+        // Try to get an integer, if possible
+        INT.fromBukkitConfig(config, path)?.toDouble()
       }
   }
 
