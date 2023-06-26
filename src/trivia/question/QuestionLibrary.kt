fda31735d7e7bf929d53955fa7895a8f1fd11892
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2023-06-26 14:43:08 -0400

    Massively improved code readability

--- src/trivia/question/QuestionLibrary.kt
+++ src/trivia/question/QuestionLibrary.kt
@@ -10,19 +10,13 @@ import org.bukkit.enchantments.Enchantment
 
 object QuestionLibrary {
 
-  // ItemStack.addEnchantment, but fluent
-  private fun ItemStack.withEnchantment(enchantment: Enchantment, level: Int): ItemStack {
-    this.addEnchantment(enchantment, level)
-    return this
-  }
-
   val QUESTIONS: List<() -> TriviaQuestion> = listOf(
     {
       MultipleChoiceQuestion(
         questionBody = "At what difficulty does the world need to be set to for zombies to break down doors?",
         answers = listOf("Peaceful", "Easy", "Medium", "Hard"),
         correctAnswerIndex = 3,
-        rewards = BlockTypes.WOODEN_DOORS.toList().map { ItemReward(it) } + listOf(ItemReward(Material.ZOMBIE_SPAWN_EGG)),
+        rewards = toRewards(BlockTypes.WOODEN_DOORS) + listOf(ItemReward(Material.ZOMBIE_SPAWN_EGG)),
         shuffleAnswers = false,
       )
     },
@@ -31,7 +25,12 @@ object QuestionLibrary {
         questionBody = "What is the most common type of armor to spawn naturally on zombies?",
         answers = listOf("Leather", "Gold", "Chainmail", "Iron", "Diamond"),
         correctAnswerIndex = 1,
-        rewards = listOf(ItemReward(Material.GOLDEN_BOOTS), ItemReward(Material.GOLDEN_HELMET), ItemReward(Material.GOLDEN_LEGGINGS), ItemReward(Material.GOLDEN_CHESTPLATE)),
+        rewards = listOf(
+          ItemReward(Material.GOLDEN_BOOTS),
+          ItemReward(Material.GOLDEN_HELMET),
+          ItemReward(Material.GOLDEN_LEGGINGS),
+          ItemReward(Material.GOLDEN_CHESTPLATE),
+        ),
       )
     },
     {
@@ -39,7 +38,7 @@ object QuestionLibrary {
         questionBody = "What was the name of the Minecraft 1.15 Update?",
         answers = listOf("World of Color", "Buzzy Bees", "Village & Pillage", "The Wild Update"),
         correctAnswerIndex = 1,
-        rewards = listOf(ItemReward(ItemStack(Material.HONEYCOMB, 4))),
+        rewards = listOf(ItemReward(ItemStack(Material.HONEYCOMB, 16))),
       )
     },
     {
@@ -47,7 +46,7 @@ object QuestionLibrary {
         questionBody = "What was the name of the Minecraft 1.12 Update?",
         answers = listOf("World of Color", "Caves & Cliffs", "Village & Pillage", "The Wild Update"),
         correctAnswerIndex = 0,
-        rewards = BlockTypes.CONCRETE_POWDERS.toList().map { ItemReward(ItemStack(it, 16)) },
+        rewards = toRewards(BlockTypes.CONCRETE_POWDERS, stackSize = 64),
       )
     },
     {
@@ -55,7 +54,7 @@ object QuestionLibrary {
         questionBody = "What was the name of the Minecraft 1.13 Update?",
         answers = listOf("Update Aquatic", "Trails & Tales", "Village & Pillage", "Wardens & Below"),
         correctAnswerIndex = 0,
-        rewards = listOf(ItemReward(Material.KELP)),
+        rewards = listOf(ItemReward(ItemStack(Material.KELP, 16))),
       )
     },
     {
@@ -71,7 +70,7 @@ object QuestionLibrary {
         questionBody = "Which of the following is NOT disabled when mobGriefing is false?",
         answers = listOf("Creepers damaging blocks", "Zombies breaking doors", "Sheep eating grass", "Skeleton horses summoning lightning"),
         correctAnswerIndex = 3,
-        rewards = listOf(ItemReward(ItemStack(Material.BONE, 8))),
+        rewards = listOf(ItemReward(ItemStack(Material.BONE, 32))),
       )
     },
     {
@@ -79,7 +78,10 @@ object QuestionLibrary {
         questionBody = "Which Minecraft update was called the Wild Update?",
         answers = listOf("1.16", "1.17", "1.18", "1.19", "1.20"),
         correctAnswerIndex = 3,
-        rewards = listOf(ItemReward(ItemStack(Material.MANGROVE_LOG, 8)), ItemReward(ItemStack(Material.MUD, 8))),
+        rewards = listOf(
+          ItemReward(ItemStack(Material.MANGROVE_LOG, 16)),
+          ItemReward(ItemStack(Material.MUD, 32)),
+        ),
         shuffleAnswers = false,
       )
     },
@@ -106,7 +108,10 @@ object QuestionLibrary {
         questionBody = "Which of these items has no smelting recipe when placed in a furnace?",
         answers = listOf("Steak", "Ancient Debris", "Iron Horse Armor", "Sea Pickle"),
         correctAnswerIndex = 0,
-        rewards = listOf(ItemReward(ItemStack(Material.COOKED_BEEF, 2)), ItemReward(Material.FURNACE)),
+        rewards = listOf(
+          ItemReward(ItemStack(Material.COOKED_BEEF, 4)),
+          ItemReward(Material.FURNACE),
+        ),
       )
     },
     {
@@ -114,7 +119,10 @@ object QuestionLibrary {
         questionBody = "Which of the following can a fox NOT eat?",
         answers = listOf("Beetroot", "Honey Bottle", "Cake", "Chorus Fruit"),
         correctAnswerIndex = 2,
-        rewards = listOf(ItemReward(Material.CAKE), ItemReward(Material.FOX_SPAWN_EGG)),
+        rewards = listOf(
+          ItemReward(Material.CAKE),
+          ItemReward(Material.FOX_SPAWN_EGG),
+        ),
       )
     },
     {
@@ -122,21 +130,33 @@ object QuestionLibrary {
         questionBody = "Which of the following will NOT appear in a desert pyramid chest?",
         answers = listOf("Bone", "Sand", "Sandstone", "Saddle"),
         correctAnswerIndex = 2,
-        rewards = listOf(ItemReward(ItemStack(Material.SANDSTONE, 64)), ItemReward(ItemStack(Material.BONE, 32)), ItemReward(Material.SADDLE)),
+        rewards = listOf(
+          ItemReward(ItemStack(Material.SANDSTONE, 64)),
+          ItemReward(ItemStack(Material.BONE, 32)),
+          ItemReward(Material.SADDLE),
+        ),
       )
     },
     {
       NumericalQuestion(
         questionBody = "How many signs can be stacked in a single inventory slot?",
         correctAnswer = 16,
-        rewards = BlockTypes.WOODEN_SIGNS.toList().map { ItemReward(it) },
+        rewards = toRewards(BlockTypes.WOODEN_SIGNS),
       )
     },
     {
       NumericalQuestion(
         questionBody = "What is the highest level of Blast Protection obtainable in a standard Minecraft game?",
         correctAnswer = 4,
-        rewards = listOf(Material.IRON_CHESTPLATE, Material.IRON_BOOTS, Material.IRON_LEGGINGS, Material.IRON_HELMET).map { EnchantedItemReward(ItemStack(it).withEnchantment(Enchantment.PROTECTION_EXPLOSIONS, 1)) },
+        rewards = run {
+          val armorMaterials = listOf(
+            Material.IRON_CHESTPLATE, Material.IRON_BOOTS,
+            Material.IRON_LEGGINGS, Material.IRON_HELMET,
+          )
+          toRewards(armorMaterials) {
+            EnchantedItemReward(it.withEnchantment(Enchantment.PROTECTION_EXPLOSIONS, 1))
+          }
+        },
       )
     }
   )
@@ -144,6 +164,31 @@ object QuestionLibrary {
   val SUPPLIER: TriviaQuestionSupplier = TriviaQuestionSupplier {
     val questionFunction = QUESTIONS.sample()!!
     questionFunction()
+  }
+
+  private fun toRewards(
+    collection: Iterable<Material>,
+    stackSize: Int,
+    block: (ItemStack) -> TriviaQuestionReward,
+  ): List<TriviaQuestionReward> =
+    collection.toList().map { ItemStack(it, stackSize).let(block) }
+
+  private fun toRewards(
+    collection: Iterable<Material>,
+    stackSize: Int = 1,
+  ): List<TriviaQuestionReward> =
+    toRewards(collection, stackSize) { ItemReward(it) }
+
+  private fun toRewards(
+    collection: Iterable<Material>,
+    block: (ItemStack) -> TriviaQuestionReward,
+  ): List<TriviaQuestionReward> =
+    toRewards(collection, stackSize = 1, block)
+
+  // ItemStack.addEnchantment, but fluent
+  private fun ItemStack.withEnchantment(enchantment: Enchantment, level: Int): ItemStack {
+    this.addEnchantment(enchantment, level)
+    return this
   }
 
 }
