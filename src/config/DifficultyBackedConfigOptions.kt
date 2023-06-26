312ee9969c92722c2fa3e0e55db006e3c151d31c
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2023-06-26 16:45:19 -0400

    config options

---
+++ src/config/DifficultyBackedConfigOptions.kt
@@ -0,0 +1,44 @@
+
+package com.mercerenies.turtletroll.config
+
+import org.bukkit.Bukkit
+
+class DifficultyBackedConfigOptions(
+  private val config: ConfigOptions,
+  private val difficultyTiers: Map<String, DifficultyTier> = DifficultyTier.MAP,
+) : ConfigOptions {
+
+  private val difficultySetting: DifficultyTier by lazy {
+    val setting = config.getValue(Class.STRING, "global.difficulty")
+    if (setting == null) {
+      Bukkit.getLogger().warning("No difficulty was set in config, assuming \"normal\"")
+      DifficultyTier.NORMAL
+    } else {
+      val tier = difficultyTiers[setting]
+      if (tier == null) {
+        Bukkit.getLogger().warning("Unknown difficulty $setting, assuming \"normal\"")
+        DifficultyTier.NORMAL
+      } else {
+        tier
+      }
+    }
+  }
+
+  // If the value is
+  // * "default"; then read from the current difficulty file
+  // * "easy", "normal", "hard", or "insane"; then read from that particular difficulty file
+  // * any other value; then take that value
+  // * absent or of the wrong type; use the default value given to us by Bukkit
+  override fun<T> getValue(classType: Class<T>, path: String): T? =
+    readFromDifficulty(classType, path) ?: config.getValue(classType, path)
+
+  private fun<T> readFromDifficulty(classType: Class<T>, path: String): T? =
+    config.getValue(Class.STRING, path).let { stringValue ->
+      when (stringValue) {
+        "default" -> difficultySetting.configOptions.getValue(classType, path)
+        in difficultyTiers -> difficultyTiers[stringValue]!!.configOptions.getValue(classType, path)
+        else -> null
+      }
+    }
+
+}
