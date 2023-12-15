fe6744f094a27f53761464201ce4f03cf5b0a4e4
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2023-12-15 12:38:55 -0500

    Bowser taunts you if you fail

--- src/demand/bowser/BowserEvent.kt
+++ src/demand/bowser/BowserEvent.kt
@@ -9,9 +9,18 @@ import net.kyori.adventure.text.Component
 
 abstract class BowserEvent : DailyDemandEvent {
 
+  companion object {
+    val GENERIC_DAY_START_MESSAGE = "<Bowser> Gwahahaha! It's me, losers! I've got a job for you all today, and you'd better listen up."
+    val GENERIC_FAILURE_MESSAGE = "<Bowser> Nightfall already. You losers couldn't even do a simple task! Maybe next time."
+  }
+
   abstract fun getDayStartMessage(): Component
 
+  open fun getFailureMessage(): Component =
+    Component.text(GENERIC_FAILURE_MESSAGE)
+
   open override fun onDayStart(godsState: GodsState) {
+    Messages.broadcastMessage(GENERIC_DAY_START_MESSAGE)
     Messages.broadcastMessage(getDayStartMessage())
   }
 
@@ -20,7 +29,10 @@ abstract class BowserEvent : DailyDemandEvent {
     // rewards you for succeeding. So if you haven't met it by
     // nightfall, just set the state to "appeased" so no punishments
     // occur.
-    godsState.setGodsAppeased(true)
+    if (!godsState.isAppeased()) {
+      Messages.broadcastMessage(getFailureMessage())
+      godsState.setGodsAppeased(true)
+    }
   }
 
 }
