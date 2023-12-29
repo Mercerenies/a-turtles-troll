f603ad48f282906bc869eb2313b9e6ca977d8863
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2023-12-29 14:33:00 -0500

    Add constants

--- src/Constants.kt
+++ src/Constants.kt
@@ -2,5 +2,9 @@
 package com.mercerenies.turtletroll
 
 object Constants {
+
   val TICKS_PER_SECOND = 20
+  val IN_GAME_HOUR_TICKS = TICKS_PER_SECOND
+  val IN_GAME_DAY_TICKS = IN_GAME_HOUR_TICKS * 24
+
 }
