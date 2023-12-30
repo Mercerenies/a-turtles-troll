e3037febd7eb36018f1a0af3ce32857ef6dc777f
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2023-12-29 19:57:36 -0500

    Debug runner for events

--- src/happening/RandomEventPool.kt
+++ src/happening/RandomEventPool.kt
@@ -18,17 +18,32 @@ class RandomEventPool(
     }
   }
 
-  fun chooseAndFireEvent(state: RandomEventState) {
+  val eventNames: List<String>
+    get() = entries.map { it.event.name }
+
+  fun chooseAndFireEvent(state: RandomEventState): RandomEvent {
     val options = entries.filter { it.event.canFire(state) }.map { it.toWeight() }
     val chosenEntry = sample(options)
+    fireEvent(chosenEntry, state)
+    return chosenEntry.event
+  }
 
+  fun fireEventByName(name: String, state: RandomEventState): RandomEvent {
+    val chosenEntry = entries.find { it.event.name == name }
+    if (chosenEntry == null) {
+      throw NoSuchElementException("No event named $name")
+    }
+    fireEvent(chosenEntry, state)
+    return chosenEntry.event
+  }
+
+  private fun fireEvent(eventEntry: RandomEventEntry, state: RandomEventState) {
     // Update the weight of all events.
     for (entry in entries) {
       entry.updateWeight()
     }
-
     // Fire the chosen event and reset its weight.
-    chosenEntry.fireAndReset(state)
+    eventEntry.fireAndReset(state)
   }
 
 }
--- src/happening/RandomEventRunnable.kt
+++ src/happening/RandomEventRunnable.kt
@@ -1,13 +1,19 @@
 
 package com.mercerenies.turtletroll.happening
 
+import com.mercerenies.turtletroll.util.component.*
 import com.mercerenies.turtletroll.feature.TurtleRunnable
 import com.mercerenies.turtletroll.command.Command
-import com.mercerenies.turtletroll.command.TerminalCommand
+import com.mercerenies.turtletroll.command.OptionalUnaryCommand
 import com.mercerenies.turtletroll.Constants
+import com.mercerenies.turtletroll.Messages
 
 import org.bukkit.command.CommandSender
 import org.bukkit.plugin.Plugin
+import org.bukkit.Bukkit
+
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
 
 // Note: RandomEventRunnable is NOT a feature. It cannot be enabled or
 // disabled. It's simply the backing engine for other features which
@@ -18,12 +24,34 @@ class RandomEventRunnable(
   private val eventPool: RandomEventPool,
 ) : TurtleRunnable(plugin), RandomEventState {
 
-  private inner class DebugFireCommand() : TerminalCommand() {
+  private inner class DebugFireCommand() : OptionalUnaryCommand() {
 
-    override fun onCommand(sender: CommandSender): Boolean {
-      run()
+    override fun onCommand(sender: CommandSender, arg: String?): Boolean {
+      val eventName: String
+      if (arg == null) {
+        // Run a random event (might be no event at all)
+        _currentTurn += 1
+        val event = eventPool.chooseAndFireEvent(this@RandomEventRunnable)
+        eventName = event.name
+      } else {
+        _currentTurn += 1
+        try {
+          eventName = arg
+          eventPool.fireEventByName(arg, this@RandomEventRunnable)
+        } catch (exc: NoSuchElementException) {
+          Messages.sendMessage(sender, Component.text("No event with name: $arg", NamedTextColor.RED))
+          Bukkit.getLogger().warning("User $sender tried to fire an event with name $arg which does not exist")
+          return false
+        }
+      }
+      Messages.sendMessage(sender, Component.text("Fired $eventName", NamedTextColor.GREEN))
       return true
     }
+
+    override fun onTabComplete(sender: CommandSender, arg: String?): List<String>? =
+      arg?.let {
+        eventPool.eventNames.filter { eventName -> eventName.startsWith(it) }
+      }
 
   }
 
