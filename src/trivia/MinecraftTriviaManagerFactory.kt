fb64981963c86339c20fb87fbb1ac2938bc1534c
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2024-01-08 12:44:26 -0500

    Simplify MinecraftTriviaManagerFactory

--- src/trivia/MinecraftTriviaManagerFactory.kt
+++ src/trivia/MinecraftTriviaManagerFactory.kt
@@ -2,7 +2,7 @@
 package com.mercerenies.turtletroll.trivia
 
 import com.mercerenies.turtletroll.feature.container.FeatureContainer
-import com.mercerenies.turtletroll.feature.container.AbstractFeatureContainer
+import com.mercerenies.turtletroll.feature.builder.FeatureBuilder
 import com.mercerenies.turtletroll.feature.builder.BuilderState
 import com.mercerenies.turtletroll.feature.builder.FeatureContainerFactory
 import com.mercerenies.turtletroll.command.withPermission
@@ -19,35 +19,18 @@ abstract class MinecraftTriviaManagerFactory() : FeatureContainerFactory<Feature
 
   }
 
-  private class Container(
-    private val manager: MinecraftTriviaManager,
-  ) : AbstractFeatureContainer() {
-
-    override val features =
-      listOf(manager)
-
-    override val commands =
-      listOf(
-        "answer" to manager.answerCommand.withPermission(Permissions.ANSWER),
-      )
-
-    override val debugCommands =
-      listOf(
-        "triviaask" to manager.triviaAskCommand,
-        "triviajudge" to manager.triviaJudgeCommand,
-      )
-
-    override val randomEvents =
-      listOf(manager.triviaRandomEvent)
-
-  }
-
   abstract val questionSupplier: TriviaQuestionSupplier
 
   override fun create(state: BuilderState): FeatureContainer {
     val config = TriviaConfig.fromGlobalConfig(state.config)
     val manager = MinecraftTriviaManager(state.plugin, config, questionSupplier)
-    return Container(manager)
+    return FeatureBuilder()
+      .addFeature(manager)
+      .addCommand("answer" to manager.answerCommand.withPermission(Permissions.ANSWER))
+      .addDebugCommand("triviaask" to manager.triviaAskCommand)
+      .addDebugCommand("triviajudge" to manager.triviaJudgeCommand)
+      .addRandomEvent(manager.triviaRandomEvent)
+      .build()
   }
 
 }
