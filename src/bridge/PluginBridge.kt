c2649413d62f228c07ea007f7d0f826a1f2d9aea
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2024-01-14 13:34:56 -0500

    PluginBridge abstract class

---
+++ src/bridge/PluginBridge.kt
@@ -0,0 +1,24 @@
+
+package com.mercerenies.turtletroll.bridge
+
+import org.bukkit.Bukkit
+import org.bukkit.NamespacedKey
+import org.bukkit.plugin.Plugin
+
+// Parent class for bridge classes to optional dependencies. Each
+// plugin that we soft-depend on (including Raccoon Mischief, which is
+// not in plugin.yml since it's a mutual dependency) has a
+// PluginBridge associated to it, which can lazily access the relevant
+// plugin if it exists in the runtime.
+abstract class PluginBridge() {
+
+  abstract val pluginName: String
+
+  val plugin: Plugin? by lazy {
+    Bukkit.getServer().getPluginManager().getPlugin(pluginName)
+  }
+
+  fun namespacedKey(key: String): NamespacedKey? =
+    plugin?.let { NamespacedKey(it, key) }
+
+}
--- src/bridge/RaccoonBridge.kt
+++ src/bridge/RaccoonBridge.kt
@@ -1,19 +1,6 @@
 
 package com.mercerenies.turtletroll.bridge
 
-import org.bukkit.Bukkit
-import org.bukkit.NamespacedKey
-import org.bukkit.plugin.Plugin
-
-object RaccoonBridge {
-
-  val PLUGIN_NAME = "RaccoonMischief"
-
-  val plugin: Plugin? by lazy {
-    Bukkit.getServer().getPluginManager().getPlugin(PLUGIN_NAME)
-  }
-
-  fun namespacedKey(key: String): NamespacedKey? =
-    plugin?.let { NamespacedKey(it, key) }
-
+object RaccoonBridge : PluginBridge() {
+  override val pluginName = "RaccoonMischief"
 }
