69801ff827f8d8f6a044073598ba49539e8b397b
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2025-01-17 18:13:37 -0500

    null implementation of CoinRewardConsumer

--- src/candy/CoinRewardConsumer.kt
+++ src/candy/CoinRewardConsumer.kt
@@ -7,4 +7,10 @@ interface CoinRewardConsumer {
   // `reason` shall be a clause which grammatically makes sense at the
   // end of "Got coins because XXX".
   fun rewardCoins(player: Player, reason: String, amount: Int)
+
+  object Null : CoinRewardConsumer {
+    override fun rewardCoins(player: Player, reason: String, amount: Int) {
+      // Do nothing
+    }
+  }
 }
